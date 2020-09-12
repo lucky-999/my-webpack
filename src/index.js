51ce39f9e4079599ada63d40df451e9323f10630
Ilya Tatarinov <itatarinov7@gmail.com>
2020-09-12 16:07:46 +0300

    index.js corrected

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/style.css';
+
+import './js/app.js';
+
+import './js/domain.js';
+
+import './js/game.js';
--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,5 @@ import Game, { GameSavingData , readGameSaving as loadGame , writeGameSaving as 
 
 const game = new Game();
 game.start();
+
+console.log('app worked')
